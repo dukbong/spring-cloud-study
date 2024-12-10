9ea32d3e9ce58039a170aebad4ccdc0280db9773
janghs <gorae930301@gmail.com>
2024-12-10 18:00:59 +0900

    Eureka 사용해서 연결 및 필터 순서

--- cloud-ex-first/src/main/java/study/cloud_ex_first/controller/FirstController.java
+++ cloud-ex-first/src/main/java/study/cloud_ex_first/controller/FirstController.java
@@ -1,13 +1,21 @@
 package study.cloud_ex_first.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String home() {
@@ -20,7 +28,8 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi! This is a message from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("httpServletRequest port number >>> {}", request.getServerPort());
+        return String.format("Hi! This is a message from First Service >>> PORT %s", env.getProperty("local.server.port"));
     }
 }
--- cloud-ex-second/src/main/java/study/cloud_ex_second/controller/SecondController.java
+++ cloud-ex-second/src/main/java/study/cloud_ex_second/controller/SecondController.java
@@ -1,14 +1,21 @@
 package study.cloud_ex_second.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
+@RequiredArgsConstructor
 public class SecondController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String home() {
@@ -21,8 +28,9 @@ public class SecondController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi! This is a message from Second Service";
+    public String check(HttpServletRequest request) {
+        log.info("httpServletRequest port number >>> {}", request.getServerPort());
+        return String.format("Hi! This is a message from Second Service >>> PORT %s", env.getProperty("local.server.port"));
     }
 
 }
