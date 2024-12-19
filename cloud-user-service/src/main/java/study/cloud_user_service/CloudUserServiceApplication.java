4f9ec55a86dc00c9fee6dfa45190fecfc740cf63
janghs <gorae930301@gmail.com>
2024-12-19 15:01:24 +0900

    설정 변경

--- cloud-user-service/src/main/java/study/cloud_user_service/CloudUserServiceApplication.java
+++ cloud-user-service/src/main/java/study/cloud_user_service/CloudUserServiceApplication.java
@@ -3,6 +3,7 @@ package study.cloud_user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // server that can be registered with the Eureka Server
